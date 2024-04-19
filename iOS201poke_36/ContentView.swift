9ba51ba3268ed9f8780d931ba1d014801860f2b6
Moe36 <23cm0136.git@jec.ac.jp>
2024-04-19 15:31:17 +0900

    Initial Commit

---
+++ iOS201poke_36/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS201poke_36
+//
+//  Created by cmStudent on 2024/04/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS201poke_36/iOS201poke_36App.swift
@@ -0,0 +1,17 @@
+//
+//  iOS201poke_36App.swift
+//  iOS201poke_36
+//
+//  Created by cmStudent on 2024/04/19.
+//
+
+import SwiftUI
+
+@main
+struct iOS201poke_36App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
